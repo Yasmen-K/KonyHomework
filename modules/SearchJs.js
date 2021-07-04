352d202f2fcac88e6b5175b0c41286cb715b6861
Yasmen-K <kodjabashevayasemin@gmail.com>
2021-07-05 00:19:41 +0300

    Jane Appleseed screen

---
+++ modules/SearchJs.js
@@ -0,0 +1,10 @@
+//Type your code here
+
+function search(){
+  let text = SearchScreen.SearchBox.text;
+  
+  if(text == 'Dome'){
+    DomeOfTheRockScreen.show();
+  }
+}
+
---
+++ userwidgets/comm.konymp.ToastSuccess/modules/require/ToastSuccessControllerActions.js
@@ -0,0 +1,11 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    /** onTouchStart defined for ImgClose **/
+    AS_Image_h23a44eda0764da2b1e9ccd375f70039: function AS_Image_h23a44eda0764da2b1e9ccd375f70039(eventobject, x, y) {
+        var self = this;
+        this.view.isVisible = false;
+        this.view.forceLayout();
+    }
+});
